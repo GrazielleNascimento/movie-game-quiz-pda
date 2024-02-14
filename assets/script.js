d5a9f3b7ec4042eb9c822a6f15296bb94048e68b
Andre Nascimento <1914andre@gmail.com>
2024-02-14 21:20:46 -0200

    fix:adicionando class as respostas

--- assets/script.js
+++ assets/script.js
@@ -14,13 +14,25 @@ const quiz = [
         pergunta: "Qual é o nome da criatura mágica que é usada para guardar o cofre do Sr. Lestrange em Gringotes, no universo de Harry Potter?",
         respostas: ["Acromântula", "Testrálio", "Hipogrifo", "Dragão Ucraniano"],
         respostaCorreta: "Dragão Ucraniano",
-        imagemRelacionada: "Hansolo.jpeg"
+        imagemRelacionada: "harry-potter.jpeg"
     },
 
+    {
+        pergunta: "No filme Harry Potter, qual é o nome do feitiço usado para abrir portas?",
+        respostas: ["Alohomora", "Expelliarmus", "Accio", "Lumos"],
+        respostaCorreta: "Alohomora",
+        imagemRelacionada: "harry-potter.jpeg"
+    },
     {
         pergunta: "Em Star Wars, qual é o nome do planeta natal de Han Solo?",
         respostas: ["Tatooine", "Endor", "Coruscant", "Corellia"],
         respostaCorreta: "Corellia",
+        imagemRelacionada: "Hansolo.jpeg"
+    },
+    {
+        pergunta: "Quem é o mestre Jedi que treinou Obi-Wan Kenobi em Star Wars?",
+        respostas: ["Yoda", "Mace Windu", "Qui-Gon Jinn", "Luke Skywalker"],
+        respostaCorreta: "Qui-Gon Jinn",
         imagemRelacionada: "Hansolo.jpeg"
     },
     {
@@ -31,26 +43,17 @@ const quiz = [
     },
 
     {
-        pergunta: "No filme Harry Potter, qual é o nome do feitiço usado para abrir portas?",
-        respostas: ["Alohomora", "Expelliarmus", "Accio", "Lumos"],
-        respostaCorreta: "Alohomora",
-        imagemRelacionada: "harry-potter.jpeg"
-    },
-    {
         pergunta: "Qual personagem de Shrek foi baseado em uma figura histórica real?",
         respostas: ["Shrek", "Burro", "Lord Farquaad", "Robin Hood"],
         respostaCorreta: "Robin Hood",
         imagemRelacionada: "shrek.jpeg"
-    },
-    {
-        pergunta: "Quem é o mestre Jedi que treinou Obi-Wan Kenobi em Star Wars?",
-        respostas: ["Yoda", "Mace Windu", "Qui-Gon Jinn", "Luke Skywalker"],
-        respostaCorreta: "Qui-Gon Jinn",
-        imagemRelacionada: "Hansolo.jpeg"
     }
+    
 ];
 
 const perguntaElement = document.querySelector('.pergunta');
 const respostasElements = document.querySelectorAll('.respostas');
 
 let indicePerguntaAtual = 0;
+
+function
