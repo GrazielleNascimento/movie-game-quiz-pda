ba64dcefbb31839ad63df6451730e4747f0ecb49
Grazielle Nascimento <90863111+GrazielleNascimento@users.noreply.github.com>
2024-02-14 17:43:54 -0300

    add elemento quiz

--- assets/script.js
+++ assets/script.js
@@ -4,3 +4,19 @@ function iniciarQuiz() {
     quizField.classList.remove('none');
     carregarPergunta();
 };
+
+
+let quizField = document.getElementById('quizField');
+let startField = document.getElementById('startField');
+
+const quiz = [
+    {
+        pergunta: "Qual é o nome da criatura mágica que é usada para guardar o cofre do Sr. Lestrange em Gringotes, no universo de Harry Potter?",
+        respostas: ["Acromântula", "Testrálio", "Hipogrifo", "Dragão Ucraniano"],
+        respostaCorreta: "Dragão Ucraniano"
+    },
+    {
+        pergunta: "Em Star Wars, qual é o nome do planeta natal de Han Solo?",
+        respostas: ["Tatooine", "Endor", "Coruscant", "Corellia"],
+        respostaCorreta: "Corellia"
+    },]
