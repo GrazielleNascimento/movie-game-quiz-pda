577432870d3d265fe885290e50c6f178c61b9450
Thiago Oliveira <thiagojunio306@gmail.com>
2024-02-14 19:31:27 -0300

    fix: adicionando propriedade de imagens relacionadas as perguntas

--- assets/script.js
+++ assets/script.js
@@ -6,40 +6,47 @@ function iniciarQuiz() {
 };
 
 
-let quizField = document.getElementById('quizField');
-let startField = document.getElementById('startField');
+let quizField = document.getElementById('campoQuiz');
+let startField = document.getElementById('campoInicio');
 
 const quiz = [
     {
         pergunta: "Qual é o nome da criatura mágica que é usada para guardar o cofre do Sr. Lestrange em Gringotes, no universo de Harry Potter?",
         respostas: ["Acromântula", "Testrálio", "Hipogrifo", "Dragão Ucraniano"],
-        respostaCorreta: "Dragão Ucraniano"
+        respostaCorreta: "Dragão Ucraniano",
+        imagemRelacionada: "Hansolo.jpeg"
     },
+
     {
         pergunta: "Em Star Wars, qual é o nome do planeta natal de Han Solo?",
         respostas: ["Tatooine", "Endor", "Coruscant", "Corellia"],
-        respostaCorreta: "Corellia"
+        respostaCorreta: "Corellia",
+        imagemRelacionada: "Hansolo.jpeg"
     },
     {
         pergunta: "Quem é o compositor da trilha sonora de Shrek?",
         respostas: ["John Williams", "Howard Shore", "Hans Zimmer", "Harry Gregson-Williams"],
-        respostaCorreta: "Harry Gregson-Williams"
+        respostaCorreta: "Harry Gregson-Williams",
+        imagemRelacionada: "shrek.jpeg"
     },
 
     {
         pergunta: "No filme Harry Potter, qual é o nome do feitiço usado para abrir portas?",
         respostas: ["Alohomora", "Expelliarmus", "Accio", "Lumos"],
-        respostaCorreta: "Alohomora"
+        respostaCorreta: "Alohomora",
+        imagemRelacionada: "harry-potter.jpeg"
     },
     {
         pergunta: "Qual personagem de Shrek foi baseado em uma figura histórica real?",
         respostas: ["Shrek", "Burro", "Lord Farquaad", "Robin Hood"],
-        respostaCorreta: "Robin Hood"
+        respostaCorreta: "Robin Hood",
+        imagemRelacionada: "shrek.jpeg"
     },
     {
         pergunta: "Quem é o mestre Jedi que treinou Obi-Wan Kenobi em Star Wars?",
         respostas: ["Yoda", "Mace Windu", "Qui-Gon Jinn", "Luke Skywalker"],
-        respostaCorreta: "Qui-Gon Jinn"
+        respostaCorreta: "Qui-Gon Jinn",
+        imagemRelacionada: "Hansolo.jpeg"
     }
 ];
 
