d5d2bc4d8c4c90c0be604e9c99d3d9bc8488548b
Thiago Oliveira <thiagojunio306@gmail.com>
2024-02-15 17:35:07 -0300

    feat: add propriedade cor de fundo e gerando cor de fundo baseado no tema

--- assets/script.js
+++ assets/script.js
@@ -5,6 +5,7 @@ const quiz = [
     respostas: ["Acromântula", "Testrálio", "Hipogrifo", "Dragão Ucraniano"],
     respostaCorreta: "Dragão Ucraniano",
     imagemRelacionada: "harry-potter.jpg",
+    corFundo: "#DEBF7E"
   },
 
   {
@@ -13,37 +14,36 @@ const quiz = [
     respostas: ["Alohomora", "Expelliarmus", "Accio", "Lumos"],
     respostaCorreta: "Alohomora",
     imagemRelacionada: "harry-potter.jpg",
+    corFundo: "#DEBF7E"
   },
   {
     pergunta: "Em Star Wars, qual é o nome do planeta natal de Han Solo?",
     respostas: ["Tatooine", "Endor", "Coruscant", "Corellia"],
     respostaCorreta: "Corellia",
     imagemRelacionada: "hansolo.jpeg",
+    corFundo: "#333a3f"
   },
   {
     pergunta: "Quem é o mestre Jedi que treinou Obi-Wan Kenobi em Star Wars?",
     respostas: ["Yoda", "Mace Windu", "Qui-Gon Jinn", "Luke Skywalker"],
     respostaCorreta: "Qui-Gon Jinn",
     imagemRelacionada: "hansolo.jpeg",
+    corFundo: "#333a3f"
   },
   {
     pergunta: "Quem é o compositor da trilha sonora de Shrek?",
-    respostas: [
-      "John Williams",
-      "Howard Shore",
-      "Hans Zimmer",
-      "Harry Gregson-Williams",
-    ],
+    respostas: ["John Williams", "Howard Shore", "Hans Zimmer", "Harry Gregson-Williams"],
     respostaCorreta: "Harry Gregson-Williams",
     imagemRelacionada: "shrek.jpg",
+    corFundo: "#cee5c3"
   },
 
   {
-    pergunta:
-      "Qual personagem de Shrek foi baseado em uma figura histórica real?",
+    pergunta: "Qual personagem de Shrek foi baseado em uma figura histórica real?",
     respostas: ["Shrek", "Burro", "Lord Farquaad", "Robin Hood"],
     respostaCorreta: "Robin Hood",
     imagemRelacionada: "shrek.jpeg",
+    corFundo: "#cee5c3"
   },
 ];
 
@@ -68,6 +68,9 @@ function carregarPergunta() {
     respostasElements[index].textContent = resposta;
   });
 
+  const body = document.body;
+  body.style.backgroundColor = perguntaAtual.corFundo;
+  
   const imagemPergunta = document.getElementById("imgPergunta");
   imagemPergunta.src = "assets/images/" + perguntaAtual.imagemRelacionada; 
 
